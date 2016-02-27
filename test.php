2439b671e1a6464214edfa4c27cfbe19f4cec433
ericdd <986387040@qq.com>
2016-02-27 10:57:40 +0800

    add test.php via github upload

---
+++ test.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<title>test</title>
+<meta name="Keywords" content="" />
+<meta name="Description" content="" />
+<style type="text/css">
+*{list-style:none;font-size:14px;font-family:tahoma;}
+#content{width:1300px;height:500px;font-size:12px;padding:3px;}
+</style>
+</head>
+<body>
+
+<pre>
+0ed628cc8f086b6fd94311aa652dd6dd
+</pre>
+
+<form method="post" action="" id="uploadform" enctype="multipart/form-data">
+
+</form>
+
+<hr />
+
+
+<pre>
+
+
+<?php
+
+echo mb_strlen("爱的aa1",'utf-8');
+
+?>
+
+
+</pre>
+
+</body>
+</html>
+
